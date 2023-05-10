dc3916db92aa0fbdf24806f33f541428e9834771
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-05-10 12:15:18 +0200

    add fibonacci with itteration

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,3 +7,15 @@ def fib_rec(num)
     fib_rec(num -1) + fib_rec(num -2)
   end
 end
+
+def fib_itteration(num)
+  array = [0, 1]
+  sum = 1
+  i = 0
+  (num - 2).times do
+    sum = sum + array[i]
+    array.push(sum)
+    i = i + 1
+  end
+  array
+end
