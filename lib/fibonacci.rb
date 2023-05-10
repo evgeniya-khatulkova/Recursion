82b9d0c802357a58ff7661ba897bbf59ad74f84e
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-05-10 19:18:12 +0200

    fix recursion fibomnacci return

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,13 +1,11 @@
-def fib_rec(num, array = [0, 1])
-  if num.zero?
-    0
-  elsif num == 1
+def fib_rec(array = [0, 1], num)
+  if num <= 1
     1
   else
     array << array[-2] + array[-1]
     fib_rec(array, num - 1)
   end
-  array
+  array[0..num - 1]
 end
 
 def fib_itteration(num)
