2dee7151211ddc164a1cac245a3aeabee8bca6f6
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-05-11 16:34:34 +0200

    improve fibonacci functioms

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,14 +1,17 @@
 def fib_rec(array = [0, 1], num)
   if num <= 1
-    1
+    return [0]
   else
     array << array[-2] + array[-1]
     fib_rec(array, num - 1)
   end
-  array[0..num - 1]
+
+  array[0..-2]
 end
 
 def fib_itteration(num)
+  return [0] if num <= 1
+
   array = [0, 1]
   sum = 1
   i = 0
