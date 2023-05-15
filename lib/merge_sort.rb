2e0c603bafac77b16f09abb52b8d62b02fa52f6b
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-05-15 16:55:24 +0200

    implemment sorting array, still needs improvement

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,11 +1,33 @@
 def merge_sort(a)
-  if a.length < 2
-    return
-  else
-    b = a[0..a.length / 2 - 1]
-    c = a[a.length / 2 .. -1]
-    merge_sort(b)
-    merge_sort(c)
+  def merge_sort(a, b = [], c =[])
+    if a.length < 2
+      return
+    else
+      b = a[0..a.length / 2 - 1]
+      c = a[a.length / 2 .. -1]
+        merge_sort(b)
+        merge_sort(c)
+      ai = 0
+      bi = 0
+      ci = 0
+      while bi < b.length && ci < c.length
+      if b[bi] < c[ci]
+        a[ai] = b[bi]
+
+        bi += 1
+      else
+            a[ai] = c[ci]
+
+            ci += 1
+      end
+             ai += 1
+      end
+      if bi < b.length - 1
+        a[ai..-1] = b[bi..b.length-1]
+      else
+         a[ai..-1] = c[ci..c.length-1]
+      end
+  end
+    a
   end
 end
-
